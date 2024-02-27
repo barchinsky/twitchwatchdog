9983eff6c6e31158e28e75b5d9bd219f63f850cd
Maksym Barchynskyi <maksym.barchynskyi@gmail.com>
2024-02-27 22:35:52 +0100

    Implemented presenting keyboard automatically when dialog is openned

--- app/src/main/java/com/m/twitchwatchdog/dashboard/addChannel/AddChannelCard.kt
+++ app/src/main/java/com/m/twitchwatchdog/dashboard/addChannel/AddChannelCard.kt
@@ -20,6 +20,8 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
@@ -28,6 +30,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
@@ -37,14 +40,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.m.twitchwatchdog.infrastructure.ui.switchRow.SwitchRow
 import com.m.twitchwatchdog.ui.theme.DarkGreyAlpha80
 import com.m.twitchwatchdog.ui.theme.TwitchWatchdogTheme
+import kotlinx.coroutines.delay
 
 @Composable
 fun AddChannelCard(
@@ -55,6 +61,8 @@ fun AddChannelCard(
     modifier: Modifier = Modifier,
 ) {
     val defaultBackgroundColor = Color.Transparent
+    val focusRequester = remember { FocusRequester() }
+    val softKeyboardController = LocalSoftwareKeyboardController.current
 
     var channelName by remember { mutableStateOf("") }
     var shouldNotify by remember { mutableStateOf(false) }
@@ -72,13 +80,20 @@ fun AddChannelCard(
 
     val animatedAddButtonAlignment by animateHorizontalAlignmentAsState(expanded)
 
+    LaunchedEffect(expanded) {
+        if (expanded) {
+            delay(500)
+            focusRequester.requestFocus()
+        }
+    }
+
     Box(
-        contentAlignment = Alignment.BottomEnd,
         modifier = Modifier
             .fillMaxSize()
             .background(animatedBackgroundColor)
             .imePadding()
             .then(modifier),
+        contentAlignment = Alignment.BottomEnd,
     ) {
         Column(
             modifier = Modifier
@@ -97,22 +112,35 @@ fun AddChannelCard(
                 Column(
                     modifier = Modifier.padding(16.dp),
                 ) {
-                    Icon(Icons.Filled.Close,
-                         contentDescription = "Close",
-                         modifier = Modifier
-                             .padding(4.dp)
-                             .align(Alignment.End)
-                             .clickable {
-                                 onCloseClicked()
-                                 channelName = ""
-                             }
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = "Close",
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .align(Alignment.End)
+                            .clickable {
+                                channelName = ""
+                                focusRequester.freeFocus()
+                                softKeyboardController?.hide()
+                                onCloseClicked()
+                            }
                     )
                     OutlinedTextField(
                         value = channelName,
                         onValueChange = { channelName = it },
                         label = { Text("Twitch channel") },
+                        keyboardActions = KeyboardActions(
+                            onDone = {
+                                onSaveChannelClicked(channelName, shouldNotify)
+                                focusRequester.freeFocus()
+                                softKeyboardController?.hide()
+                                channelName = ""
+                            }
+                        ),
+                        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
                         modifier = Modifier
-                            .fillMaxWidth(),
+                            .fillMaxWidth()
+                            .focusRequester(focusRequester),
                     )
                     Spacer(modifier = Modifier.heightIn(8.dp))
                     SwitchRow(
@@ -127,7 +155,12 @@ fun AddChannelCard(
                 expanded = expanded,
                 enabled = isSaveEnabled,
                 onAddChannelClick = onAddChannelClicked,
-                onSaveChannelClick = { onSaveChannelClicked(channelName, shouldNotify) },
+                onSaveChannelClick = {
+                    onSaveChannelClicked(channelName, shouldNotify)
+                    focusRequester.freeFocus()
+                    softKeyboardController?.hide()
+                    channelName = ""
+                },
                 modifier = Modifier
                     .padding(end = 16.dp, bottom = 16.dp)
                     .systemBarsPadding()
