94661045ea14b6f8ed4fdaf743c23d17a8595ac0
Maksym Barchynskyi <maksym.barchynskyi@gmail.com>
2024-02-28 16:25:31 +0100

    Added animation to swipe to delete row background

--- app/src/main/java/com/m/twitchwatchdog/ui/swipeToDismiss/SwipeToDismissBackground.kt
+++ app/src/main/java/com/m/twitchwatchdog/ui/swipeToDismiss/SwipeToDismissBackground.kt
@@ -1,9 +1,12 @@
 package com.m.twitchwatchdog.ui.swipeToDismiss
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.DismissDirection
@@ -12,6 +15,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,13 +29,19 @@ fun SwipeToDismissBackground(
 
     val color = MaterialTheme.colorScheme.error
         .takeIf { state.dismissDirection == DismissDirection.EndToStart }
-        ?: Color.Transparent
+        ?: MaterialTheme.colorScheme.background
+
+    val animatedBackgroundColor by animateColorAsState(
+        targetValue = color,
+        label = "Swipe to dismiss row background color",
+        animationSpec = tween(500)
+    )
 
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(color)
-            .padding(16.dp),
+            .background(animatedBackgroundColor, RoundedCornerShape(6.dp))
+            .padding(8.dp),
         contentAlignment = Alignment.CenterEnd,
     ) {
         Icon(imageVector = Icons.Filled.Delete, contentDescription = null, tint = Color.White)
